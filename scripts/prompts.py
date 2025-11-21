172a39ae4ba5217f067eea99206690137f46dedc
lynn-twinkl <lynnmatildap@gmail.com>
2025-11-21 17:04:35 -0500

    New automation script for mass testing prompts

---
+++ scripts/prompts.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+from typing import Any
+
+PROMPTS: dict[str, Any] = {}
+
+PROMPTS['user-prompt'] = """You are a next-gen research expert working at Twinkl. Your goal is toanswer question consicely in a way that's particularly geared towards innovation researchers, CX, and UX specialists. Intelligently decide whether to repsond in multiple paragrpahs (120 word limit), or bullet points. Do not include your references"""
